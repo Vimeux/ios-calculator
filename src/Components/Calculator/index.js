c55bb02e32a666913734d5cc9295bc53b26f51f1
hugo@122 <hugo@scorenco.com>
2023-03-01 15:01:16 +0100

    Stylisation de la calculatrice

--- src/Components/Calculator/index.js
+++ src/Components/Calculator/index.js
@@ -1,64 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Calculator() {
   return (
-    <div className="calculator">
-      <div className="display">result</div>
-      <div className="keypad">
-        <button className="key clear">
-          CE
-        </button>
-        <button className="key backspace">
-          C
-        </button>
-        <button className="key operator" name="/">
-          ÷
-        </button>
-        <button className="key" name="7">
-          7
-        </button>
-        <button className="key" name="8">
-          8
-        </button>
-        <button className="key" name="9">
-          9
-        </button>
-        <button className="key operator" name="*">
-          ×
-        </button>
-        <button className="key" name="4">
-          4
-        </button>
-        <button className="key" name="5">
-          5
-        </button>
-        <button className="key" name="6">
-          6
-        </button>
-        <button className="key operator" name="-">
-          −
-        </button>
-        <button className="key" name="1">
-          1
-        </button>
-        <button className="key" name="2">
-          2
-        </button>
-        <button className="key" name="3">
-          3
-        </button>
-        <button className="key operator" name="+">
-          +
-        </button>
-        <button className="key zero" name="0">
-          0
-        </button>
-        <button className="key" name=".">
-          .
-        </button>
-        <button className="key equals">
-          =
-        </button>
+    <div className="content">
+      <div className="calculator">
+        <div className="display">result</div>
+        <div className="keypad">
+          <button className="key clear">CE</button>
+          <button className="key backspace">C</button>
+          <button className="key" name="7">
+            7
+          </button>
+          <button className="key operator" name="/">
+            ÷
+          </button>
+          <button className="key" name="8">
+            8
+          </button>
+          <button className="key" name="9">
+            9
+          </button>
+          <button className="key" name="4">
+            4
+          </button>
+          <button className="key operator" name="*">
+            ×
+          </button>
+          <button className="key" name="5">
+            5
+          </button>
+          <button className="key" name="6">
+            6
+          </button>
+          <button className="key" name="1">
+            1
+          </button>
+          <button className="key operator" name="-">
+            −
+          </button>
+          <button className="key" name="2">
+            2
+          </button>
+          <button className="key" name="3">
+            3
+          </button>
+          <button className="key" name=".">
+            .
+          </button>
+          <button className="key operator" name="+">
+            +
+          </button>
+          <button className="key zero" name="0">
+            0
+          </button>
+          <button className="key equals">=</button>
+        </div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Calculator from './components/Calculator';
+import React from "react";
+import ReactDOM from "react-dom";
+import Calculator from "./components/Calculator";
+import "./styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Calculator />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
